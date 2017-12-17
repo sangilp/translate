449f0179fd830ec8eae7390553992d4b127971a6
sangilp <33713222+sangilp@users.noreply.github.com>
2017-12-17 20:56:36 +0900

    #11 번역 전, 후 언어 선택버튼 추가

--- app/src/main/java/com/example/ducksang/translation/MainActivity.java
+++ app/src/main/java/com/example/ducksang/translation/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.ducksang.translation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.StringBuilderPrinter;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,7 +26,14 @@ import java.net.URLEncoder;
 public class MainActivity extends AppCompatActivity {
     EditText bfText;
     TextView afText;
-    Button transButton;
+    TextView langOne;
+    TextView langTwo;
+    ImageButton transButton;
+    ImageButton srcButton;
+    ImageButton tarButton;
+
+    String srcLang = "en";
+    String tarLang = "ko";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,15 @@ public class MainActivity extends AppCompatActivity {
 
         bfText = (EditText) findViewById(R.id.before_text);
         afText = (TextView) findViewById(R.id.after_text);
-        transButton = (Button) findViewById(R.id.trans_button);
+        transButton = (ImageButton) findViewById(R.id.translateButton);
+        srcButton = (ImageButton) findViewById(R.id.sourceButton);
+        tarButton = (ImageButton) findViewById(R.id.targetButton);
+        langOne = (TextView) findViewById(R.id.langOne);
+        langTwo = (TextView) findViewById(R.id.langTwo);
+
+        // 초기 선택되어 있는 언어 출력
+        langOne.setText("영어");
+        langTwo.setText("한국어");
 
         // 번역실행 클릭했을때 발생할 이벤트 구현
         transButton.setOnClickListener(new View.OnClickListener() {
@@ -55,17 +70,88 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-     }
 
+        // 번역 할 언어 선택 이벤트
+        srcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String[] items = new String[]{"한국어", "영어", "일본어", "중국어간체", "중국어번체"};
+                final int[] selectedIndex = {0};
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("번역 전 언어 선택");
+                dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        selectedIndex[0] = i;
+                    }
+                });
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, items[selectedIndex[0]]+"가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                        switch (selectedIndex[0]){
+                            case 0:
+                                srcLang="ko"; langOne.setText(items[selectedIndex[0]]); return;
+                            case 1:
+                                srcLang="en"; langOne.setText(items[selectedIndex[0]]); return;
+                        }
+                        langOne.setText(srcLang);
+                    }
+                });
+                dialog.create().show();
+            }
+        });
+
+
+        // 번역 후 언어 선택 이벤트
+        tarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String[] items = new String[]{"한국어", "영어", "일본어", "중국어간체", "중국어번체", "프랑스어"};
+                final int[] selectedIndex = {0};
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("번역 후 언어 선택");
+                dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        selectedIndex[0] = i;
+                    }
+                });
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, items[selectedIndex[0]]+"가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                        switch (selectedIndex[0]){
+                            case 0:
+                                tarLang="ko"; langTwo.setText(items[selectedIndex[0]]); return;
+                            case 1:
+                                tarLang="en"; langTwo.setText(items[selectedIndex[0]]); return;
+                            case 2:
+                                tarLang="ja"; langTwo.setText(items[selectedIndex[0]]); return;
+                            case 3:
+                                tarLang="zh-CN"; langTwo.setText(items[selectedIndex[0]]); return;
+                            case 4:
+                                tarLang="zh-TW"; langTwo.setText(items[selectedIndex[0]]); return;
+                            case 5:
+                                tarLang="fr"; langTwo.setText(items[selectedIndex[0]]); return;
+                        }
+                    }
+                });
+                dialog.create().show();
+            }
+        });
+    }
+
+
+    // Naver Open API 사용
     public class NaverTranslateTask extends AsyncTask<String, Void, String> {
 
         public String resultText;
         // Naver Open API client 연동
         String clientId = "xUmNqbKm8ykD9sC4P_b0";
         String clientSecret = "h1coWEbBMP";
-
-        String srcLang = "en";
-        String tarLang = "ko";
 
         @Override
         protected void onPreExecute() {
@@ -137,5 +223,23 @@ public class MainActivity extends AppCompatActivity {
                 return translatedText;
             }
         }
+    }
+
+
+    // 뒤로가기 확인
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("알림");
+        builder.setMessage("앱을 종료하시겠습니까?");
+        builder.setPositiveButton("종료", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
+        builder.setNegativeButton("취소", null);
+
+        builder.show();
     }
 }
